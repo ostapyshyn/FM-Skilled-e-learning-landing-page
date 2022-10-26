95177ad460e4012861fa419f1f289b36ff23b704
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-10-26 22:52:19 +0300

    Fix tablet styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../Button';
 import styles from './About.module.scss';
 
 function About({ backColor, dark, btnColor }) {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div style={{ backgroundColor: `var(${backColor})` }} className={styles.about_info}>
+    <div
+      style={{
+        backgroundColor: `var(${backColor})`,
+        height: dark ? '120px' : windowSize <= 1024 ? '90px' : '104px',
+      }}
+      className={styles.about_info}>
       <div className={styles.about}>
-        <span style={{ color: dark ? '#fff' : '#13183F' }}>skilled</span>
+        <span
+          style={{
+            color: dark ? '#fff' : '#13183F',
+          }}>
+          skilled
+        </span>
         <Button btnColor={btnColor}>Get Started</Button>
       </div>
     </div>
